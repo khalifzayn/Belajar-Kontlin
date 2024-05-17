4d10e6af5f6dd570e7d94de56deea8544dc9daff
Faisal Zuhri <faisalzero.fz@gmail.com>
2024-05-17 21:50:47 +0700

    feat/FSZ-002: Latihan Kedua menggunakan Val

---
+++ src/main/kotlin/LatihanKedua.kt
@@ -0,0 +1,20 @@
+fun main() {
+    /**
+     * Penggunaan Val (Read-Only Variabel)
+     *
+     * Val digunakan untuk mendeklarasikan variabel yang nilainya tidak bisa diubah
+     * setelah dideklarasikan. Variabel ini mirip dengan final di java
+     *
+     * @author: Faisal Zuhri
+     * */
+
+    val name = "Faisal Zuhri" // Variabel yang hanya dapat dibaca bertipe String
+    println(name) // Output: Arfan
+
+    // name = "Zuhri" // ini akan menyebabkan error saat kompilasi
+
+    val age = 25 // Variabel yang hanya dapat dibaca bertipe Int
+    println(age) // Output:25
+
+    // age = 26 // ini akan menyebabkan error saat kompilasi
+}
