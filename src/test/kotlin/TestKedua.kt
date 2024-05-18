a43f37dfc93137fa93db414d67662a18d8a53eb4
Faisal Zuhri <faisalzero.fz@gmail.com>
2024-05-18 11:40:15 +0700

    feat/FSZ-002: Test Kedua menggunakan Val

---
+++ src/test/kotlin/TestKedua.kt
@@ -0,0 +1,20 @@
+fun main() {
+    /**
+     * Penggunaan Val (Read-Only Variabel)
+     *
+     * Val digunakan untuk mendeklarasikan variabel yang nilainya tidak bisa diubah
+     * setelah di deklarasikan. Variabel ini mirip dengan final Java
+     *
+     * @author: Faisal Zuhri
+     * */
+
+    val name ="Faisal Zuhri" // Variabel yang hanya dapat dibaca bertipe String
+    println(name) // Output: Arfan
+
+    // name = "Zuhri" // ini akan menyebabkan error saat kompilasi
+
+    val age = 25 // Variabel yang dapat dibaca bertipe Int
+    println(age) // Output: 25
+
+    // age = 26 // ini akan menyebabkan error saat kompilasi
+}
