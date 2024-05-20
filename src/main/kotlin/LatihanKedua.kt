818d58d1b3ce87f7200190edb3651c7e29b2200b
khalifzayn <30954990+khalifzayn@users.noreply.github.com>
2024-05-20 20:41:16 +0700

    [FSZ 002] feat: latihan kedua (#4)
    * feat/FSZ-002: Test Kedua menggunakan Val
    
    * feat/FSZ-002: Test Kedua menggunakan Val
    
    * feat/FSZ-002: Remove file test kedua

--- src/main/kotlin/LatihanKedua.kt
+++ src/main/kotlin/LatihanKedua.kt
@@ -1,20 +1,20 @@
 fun main() {
     /**
-     * Penggunaan Val (Read-Only Variabel)
+     *Penggunaan Val (Read-Only Variabel)
      *
      * Val digunakan untuk mendeklarasikan variabel yang nilainya tidak bisa diubah
-     * setelah dideklarasikan. Variabel ini mirip dengan final di java
+     * setelah di deklarasikan. Variabel ini mirip dengan final di Java
      *
      * @author: Faisal Zuhri
      * */
 
-    val name = "Faisal Zuhri" // Variabel yang hanya dapat dibaca bertipe String
+    val name ="Faisal Zuhri" // Variabel yang hanya dapat dibaca bertipe String
     println(name) // Output: Arfan
 
     // name = "Zuhri" // ini akan menyebabkan error saat kompilasi
 
-    val age = 25 // Variabel yang hanya dapat dibaca bertipe Int
-    println(age) // Output:25
+    val age = 25 //Variabel yang hanya dapat dibaca bertipe Int
+    println(age) // Output: 25
 
     // age = 26 // ini akan menyebabkan error saat kompilasi
 }
